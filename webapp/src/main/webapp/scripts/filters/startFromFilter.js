945e512321bec6183603598148609a293980b6c2
Rafael <Rafael@192.168.25.86>
2017-08-27 23:49:26 -0300

    atualização ui

--- webapp/src/main/webapp/scripts/filters/startFromFilter.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('professor-service').filter('startFrom', function() {
-    return function(input, start) {
-        start = +start; //parse to int
-        return input.slice(start);
-    };
-});
--- webapp/src/main/webapp/scripts/services/ProfessorFactory.js
+++
@@ -1,6 +0,0 @@
-angular.module('professor-service').factory('ProfessorResource', function($resource){
-	var resource = $resource('http://192.168.25.86:8085/rest/professores/:ProfessorId',{ProfessorId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
-	var Topology = topology();
-	Topology.ajax("professor-service","/rest/professores");
-	return resource;
-});
